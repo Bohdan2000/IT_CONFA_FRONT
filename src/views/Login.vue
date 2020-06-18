fc4968fb1176bb7e19ca1ded9bf0a029bbf0d5cb
Bohdan__Mayorchak <bohdan.ma@starkey.co.il>
2020-06-18 14:29:21 +0300

    first commit

---
+++ src/views/Login.vue
@@ -0,0 +1,61 @@
+<template>
+  <form class="card auth-card">
+    <div class="card-content">
+      <span class="card-title center">IT CONFA</span>
+      <div class="center">
+          <label class="role-radio">
+            <input
+                class="with-gap"
+                name="type"
+                type="radio"
+                value="income"
+                checked="checked"
+            />
+            <span>User</span>
+          </label>
+          <label class="role-radio">
+            <input
+                class="with-gap"
+                name="type"
+                type="radio"
+                value="outcome"
+            />
+            <span>Admin</span>
+          </label>
+      </div>
+      <div class="input-field">
+        <input
+            id="email"
+            type="text"
+            class="validate"
+        >
+        <label for="email">Email</label>
+        <small class="helper-text invalid">Email</small>
+      </div>
+      <div class="input-field">
+        <input
+            id="password"
+            type="password"
+            class="validate"
+        >
+        <label for="password">Password</label>
+        <small class="helper-text invalid">Password</small>
+      </div>
+    </div>
+    <div class="card-action">
+      <div>
+        <button
+            class="btn waves-effect waves-light auth-submit"
+            type="submit"
+        >
+          Login
+          <i class="material-icons right">send</i>
+        </button>
+      </div>
+
+      <p class="center">
+        <a href="/register">SIGN UP</a>
+      </p>
+    </div>
+  </form>
+</template>
---
+++ src/views/Register.vue
@@ -0,0 +1,80 @@
+<template>
+  <form class="card auth-card">
+    <div class="card-content">
+      <span class="card-title center">IT CONFA</span>
+      <div class="input-field">
+        <input
+            id="email"
+            type="text"
+        >
+        <label for="email">Email</label>
+        <small class="helper-text invalid">Email</small>
+      </div>
+      <div class="input-field">
+        <input
+            id="password"
+            type="password"
+            class="validate"
+        >
+        <label for="password">Password</label>
+        <small class="helper-text invalid">Password</small>
+      </div>
+      <div class="input-field">
+        <input
+            id="FirstName"
+            type="text"
+            class="validate"
+        >
+        <label for="FirstName">First name</label>
+        <small class="helper-text invalid">First name</small>
+      </div>
+      <div class="input-field">
+        <input
+            id="LastName"
+            type="text"
+            class="validate"
+        >
+        <label for="LastName">Last name</label>
+        <small class="helper-text invalid">Last name</small>
+
+      </div>
+    </div>
+    <div class="center margin-role-div">
+          <label class="role-radio">
+            <input
+                class="with-gap"
+                name="type"
+                type="radio"
+                value="income"
+                checked="checked"
+            />
+            <span>User</span>
+          </label>
+          <label class="role-radio">
+            <input
+                class="with-gap"
+                name="type"
+                type="radio"
+                value="outcome"
+            />
+            <span>Admin</span>
+          </label>
+      </div>
+    <div class="card-action">
+      <div>
+        <button
+            class="btn waves-effect waves-light auth-submit"
+            type="submit"
+        >
+          SIGN UP
+          <i class="material-icons right">send</i>
+        </button>
+      </div>
+
+      <p class="center">
+        Alredy have account?
+        <a href="/login">Login!</a>
+      </p>
+    </div>
+  </form>
+</template>
